bd11335a2193de09358b8d95fa8b542ec5e79f20
handesaurabh <handesaurabh2004@gmail.com>
2025-12-17 15:06:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,29 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchLoremData } from "./redux/actions/loremActions";
 
 const App = () => {
+  const dispatch = useDispatch();
+  const { loading, data } = useSelector(state => state.lorem);
+
+  useEffect(() => {
+    dispatch(fetchLoremData());
+  }, [dispatch]);
+
   return (
-    <div style={{ maxWidth: '800px', margin: '0 auto', backgroundColor: 'white', padding: '20px', borderRadius: '8px', boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)' }}>
-      <h1 style={{ color: '#333', textAlign: 'center' }}>Lorem Ipsum Data</h1>
-      <div>
-        <h2 style={{ color: '#555', marginTop: '20px' }}>Welcome to Lorem Ipsum Generator</h2>
-        <p style={{ lineHeight: '1.6', color: '#666' }}>This is a React & Redux application that fetches Lorem Ipsum text.</p>
-        <p style={{ textAlign: 'center', color: '#888', fontStyle: 'italic' }}>Fetching data...</p>
-      </div>
+    <div style={{ maxWidth: "800px", margin: "0 auto" }}>
+      <h1>A short Naration of Lorem Ipsum</h1>
+
+      {loading && <h4>Loading...</h4>}
+
+      {!loading && data?.title && (
+        <ul>
+          <li>
+            <h4>{data.title}</h4>
+            <p>{data.body}</p>
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
