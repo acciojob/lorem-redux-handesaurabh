a244773b93faa27b1aac63bed410a9e6bafcde91
handesaurabh <handesaurabh2004@gmail.com>
2025-12-17 14:57:05 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import App from "./components/App";
+import store from "./store/configureStore";
 
+console.log('Redux store:', store);
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
