3463a98aa6600ac36d6993bbfab13c2dc6b56996
handesaurabh <handesaurabh2004@gmail.com>
2025-12-17 15:52:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import "regenerator-runtime/runtime";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import App from "./components/App";
-import store from "./store/configureStore";
+import App from "./components/App.js";
+import store from "./store/configureStore.js";
 
 console.log('Redux store:', store);
 
