16a003405dc199e17f001bba57ecbc9bcc3f3c6f
handesaurabh <handesaurabh2004@gmail.com>
2025-12-17 16:01:47 +0530

    Update loremReducer.js

--- src/reducers/loremReducer.js
+++ src/reducers/loremReducer.js
@@ -7,18 +7,14 @@ const initialState = {
 };
 
 const loremReducer = (state = initialState, action) => {
-  console.log('Reducer called with:', { state, action });
-  
   switch (action.type) {
     case FETCH_LOREM_REQUEST:
-      console.log('Handling FETCH_LOREM_REQUEST');
       return {
         ...state,
         loading: true,
         error: ''
       };
     case FETCH_LOREM_SUCCESS:
-      console.log('Handling FETCH_LOREM_SUCCESS with payload:', action.payload);
       return {
         ...state,
         loading: false,
@@ -26,7 +22,6 @@ const loremReducer = (state = initialState, action) => {
         error: ''
       };
     case FETCH_LOREM_FAILURE:
-      console.log('Handling FETCH_LOREM_FAILURE with payload:', action.payload);
       return {
         ...state,
         loading: false,
