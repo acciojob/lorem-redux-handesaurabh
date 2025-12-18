f096a93ee07fce4b64e388e027bbce076f093476
handesaurabh <handesaurabh2004@gmail.com>
2025-12-18 23:29:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchLoremData } from '../actions/loremActions';
-import './styles/App.css';
+import '../styles/App.css';
 
 const App = () => {
   const dispatch = useDispatch();
