2a119adc2fba4f661f0d3173f47138a4855b3623
handesaurabh <handesaurabh2004@gmail.com>
2025-12-18 23:15:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import "regenerator-runtime/runtime";
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import App from "./components/App";
-import store from "./store/configureStore";
-
-console.log('Redux store:', store);
+import 'regenerator-runtime/runtime';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import store from './store/configureStore';
+import './styles.css';
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
