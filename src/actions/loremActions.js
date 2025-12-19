64548e6d411d5ed1326e37fd1011b96f90fc1c52
handesaurabh <handesaurabh2004@gmail.com>
2025-12-19 18:53:13 +0530

    Update loremActions.js

--- src/actions/loremActions.js
+++ src/actions/loremActions.js
@@ -66,7 +66,7 @@ export const fetchLoremData = () => {
       console.warn('API not available, using mock data:', error.message);
       const mockData = [{
         title: 'Lorem Ipsum Dolor Sit Amet',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+        body: '1'
       }];
       console.log('Using mock data:', mockData);
       dispatch(fetchLoremSuccess(mockData));
