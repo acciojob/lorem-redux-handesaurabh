e21e45321c84d66515e56492f57ebb9eba3e8105
handesaurabh <handesaurabh2004@gmail.com>
2025-12-19 18:50:15 +0530

    Update loremReducer.js

--- src/reducers/loremReducer.js
+++ src/reducers/loremReducer.js
@@ -1,8 +1,4 @@
-import {
-  FETCH_LOREM_REQUEST,
-  FETCH_LOREM_SUCCESS,
-  FETCH_LOREM_FAILURE
-} from '../actions/loremActions';
+import { FETCH_LOREM_REQUEST, FETCH_LOREM_SUCCESS, FETCH_LOREM_FAILURE } from '../actions/loremActions';
 
 const initialState = {
   loading: true,
